c7fbc8cd2081519b940275f5c7a318d219d0ef3f
Joe <mrdrawin983@gmail.com>
2025-08-11 16:39:20 +0200

    cleaning the code

---
+++ go-tutorial/cmd/tutorial/code-collection.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+// constants and variables basics
+func foo() string {
+	return "Hello from foo!"
+}
+func constVarsBasics() {
+	var intNum int = 32767
+	intNum = intNum + 1
+	fmt.Println(intNum)
+
+	var floatNum float64 = 123123123.9
+	fmt.Println(floatNum)
+
+	var floatNum32 float32 = 10.1
+	var intNum32 int32 = 2
+	var result float32 = floatNum32 * float32(intNum32)
+	fmt.Println("Result of float32 * int32:", result)
+
+	var intNum1 int = 3
+	var intNum2 int = 2
+	fmt.Println(intNum1 / intNum2)
+	fmt.Println(intNum1 % intNum2)
+
+	var myString string = "Hello" + " " + "World"
+	fmt.Println(myString)
+
+	fmt.Println(utf8.RuneCountInString("y"))
+
+	var myRune rune = 'a'
+	fmt.Println(myRune)
+
+	var myBoolean bool = false
+	fmt.Println(myBoolean)
+
+	var intNum3 rune
+	fmt.Println(intNum3)
+
+	var myVar string = foo()
+	fmt.Println(myVar)
+
+	var1, var2 := 1, 2
+	fmt.Println(var1, var2)
+
+	const myConst string = "const value"
+	fmt.Println(myConst)
+}
+
+//
+
+func main() {
+
+}
